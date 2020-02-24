5802f4144ae94c814332bb688d7e22226828e2af
saveav <adam.pahlevi@gmail.com>
2020-02-25 00:05:51 +0900

    user model

---
+++ chapter3/tandibi/app/models/user.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  email      :string           not null
+#  first_name :string           not null
+#  is_public  :boolean          default("true"), not null
+#  last_name  :string
+#  username   :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email     (email) UNIQUE
+#  index_users_on_username  (username) UNIQUE
+#
+class User < ApplicationRecord
+end
---
+++ chapter3/tandibi/spec/models/user_spec.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  email      :string           not null
+#  first_name :string           not null
+#  is_public  :boolean          default("true"), not null
+#  last_name  :string
+#  username   :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email     (email) UNIQUE
+#  index_users_on_username  (username) UNIQUE
+#
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
