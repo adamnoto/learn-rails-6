ac681f792688f01d2ceb476f9e787c5c8bf7e50c
saveav <adam.pahlevi@gmail.com>
2020-03-03 13:35:56 +0900

    use scope for Bond

--- chapter3/tandibi/app/models/bond.rb
+++ chapter3/tandibi/app/models/bond.rb
@@ -35,4 +35,8 @@ class Bond < ApplicationRecord
 
   belongs_to :user
   belongs_to :friend, class_name: "User"
+
+  scope :following, -> { where(state: FOLLOWING) }
+  scope :requesting, -> { where(state: REQUESTING) }
+  scope :blocking, -> { where(state: BLOCKING) }
 end
--- chapter3/tandibi/app/models/user.rb
+++ chapter3/tandibi/app/models/user.rb
@@ -29,12 +29,12 @@ class User < ApplicationRecord
   has_many :bonds
 
   has_many :followings,
-    -> { where("bonds.state = ?", Bond::FOLLOWING) },
+    -> { Bond.following },
     through: :bonds,
     source: :friend
 
   has_many :follow_requests,
-    -> { where("bonds.state = ?", Bond::REQUESTING) },
+    -> { Bond.requesting },
     through: :bonds,
     source: :friend
 
@@ -43,7 +43,7 @@ class User < ApplicationRecord
     foreign_key: :friend_id
 
   has_many :followers,
-    -> { where("bonds.state = ?", Bond::FOLLOWING) },
+    -> { Bond.following },
     through: :inward_bonds,
     source: :user
 
