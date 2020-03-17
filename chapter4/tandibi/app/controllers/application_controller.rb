a03f37477736c1595d1ede4303c974194c420e03
Adam Notodikromo <adam.pahlevi@gmail.com>
2020-03-18 07:38:47 +0900

    setting up layout

--- chapter4/tandibi/app/controllers/application_controller.rb
+++ chapter4/tandibi/app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
+
+  private
+
+    def layout_by_resource
+      devise_controller? ? "session" : "application"
+    end
+
 end
--- chapter4/tandibi/app/javascript/css/tailwind.js
+++ chapter4/tandibi/app/javascript/css/tailwind.js
@@ -1,6 +1,12 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+      }
+    },
   },
   variants: {},
   plugins: [],
