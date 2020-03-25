c6cb7f72f75e0a4ccc1d8223742cb15f940620b9
Adam Notodikromo <adam.pahlevi@gmail.com>
2020-03-25 20:26:18 +0900

    styling email

--- chapter4/tandibi/config/application.rb
+++ chapter4/tandibi/config/application.rb
@@ -31,5 +31,9 @@ module Tandibi
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.to_prepare do
+      Devise::Mailer.layout "mailer"
+    end
   end
 end
