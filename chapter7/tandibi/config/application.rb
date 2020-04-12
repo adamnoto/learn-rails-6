152bd4d0454e97de542a69bdffdc5d31c31b0bbd
Adam Notodikromo <adam.pahlevi@gmail.com>
2020-04-13 06:17:28 +0900

    set up the job's backend: delayed job

--- chapter7/tandibi/config/application.rb
+++ chapter7/tandibi/config/application.rb
@@ -32,6 +32,8 @@ module Tandibi
     # Don't generate system test files.
     config.generators.system_tests = nil
 
+    config.active_job.queue_adapter = :delayed_job
+
     config.to_prepare do
       Devise::Mailer.layout "mailer"
     end
--- chapter7/tandibi/db/schema.rb
+++ chapter7/tandibi/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_09_212000) do
+ActiveRecord::Schema.define(version: 2020_04_12_211317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,21 @@ ActiveRecord::Schema.define(version: 2020_04_09_212000) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id", "friend_id"], name: "index_bonds_on_user_id_and_friend_id", unique: true
+  end
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer "priority", default: 0, null: false
+    t.integer "attempts", default: 0, null: false
+    t.text "handler", null: false
+    t.text "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string "locked_by"
+    t.string "queue"
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
   end
 
   create_table "pictures", force: :cascade do |t|
